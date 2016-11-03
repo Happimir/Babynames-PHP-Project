6bddc13c450fb52f2de9e961de885031ea731ca4
neverritt <neverritt@gmail.com>
2016-11-02 20:39:09 -0400

    semantic change

--- names.js
+++ names.js
@@ -1,10 +1,12 @@
 /* names.js */
 
 /* Fetches the PHP meaning query and populates the meaning paragraph */
-function meaning();
+function meaning(){
+}
 
 /* Fetches the PHP rank query and populates the table if the name exists */
-function rank();
+function rank(){
+}
 
 /* Search button calls the meaning and rank functions and allows subsequent searches */
 document.getElementById("search").addEventListener("click", function(){
