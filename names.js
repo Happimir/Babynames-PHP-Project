5c64e8ba3cf40dfe9fa583add05493ab801a8e3e
neverritt <neverritt@gmail.com>
2016-11-02 20:36:43 -0400

    Created names.js with prototype functions

---
+++ names.js
@@ -0,0 +1,13 @@
+/* names.js */
+
+/* Fetches the PHP meaning query and populates the meaning paragraph */
+function meaning();
+
+/* Fetches the PHP rank query and populates the table if the name exists */
+function rank();
+
+/* Search button calls the meaning and rank functions and allows subsequent searches */
+document.getElementById("search").addEventListener("click", function(){
+	meaning();
+	rank();
+});
